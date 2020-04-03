9e8b000b481c61a5d3c16e6c2257e99502112e32
marat <m.gaynutdinov@mail.ru>
2020-04-03 23:04:04 +0300

    Completed chapter08

--- chapter_07/Activity_7.01/main.go
+++ chapter_07/Activity_7.01/main.go
@@ -1,97 +1,101 @@
 package main
-import (
+import (	
+	"errors"
 	"fmt"
+	"os"
 )
-
-type Employee struct {
-	Id			int
-	FirstName	string
-	LastName	string
-}
-
-type Developer struct {
-	Individual			Employee
-	HourlyRate			int
-	HoursWorkedInYear	string
-	Review				map[string]interface{}
-}
-
-type Manager struct {
-	Individual			Employee
-	Salary				int
-	CommissionRate		int
-}
-
 type Payer interface {
-	Pay() string, int
+	Pay() (string, float64)
 }
-
-func (d Developer) Pay() string, int{
-	return d.FirstName, d.HourlyRate * d.DeveloperHoursWorkInYear
+type Developer struct {
+	Individual        Employee
+	HourlyRate        float64
+	HoursWorkedInYear float64
+	Review            map[string]interface{}
 }
-
-func (m Manager) Pay() string, int{
-	return m.FirstName, m.Salary + (m.Salary * m.CommissionRate)
+type Employee struct {
+	Id        int
+	FirstName string
+	LastName  string
 }
-
+type Manager struct {
+	Individual     Employee
+	Salary         float64
+	CommissionRate float64
+}
+func main() {
+	employeeReview := make(map[string]interface{})
+	employeeReview["WorkQuality"] = 5
+	employeeReview["TeamWork"] = 2
+	employeeReview["Communication"] = "Poor"
+	employeeReview["Problem-solving"] = 4
+	employeeReview["Dependability"] = "Unsatisfactory"
+	d := Developer{Individual: Employee{Id: 1, FirstName: "Eric", LastName: "Davis"}, HourlyRate: 35, HoursWorkedInYear: 2400, Review: employeeReview}
+	m := Manager{Individual: Employee{Id: 2, FirstName: "Mr.", LastName: "Boss"}, Salary: 150000, CommissionRate: .07}
+err := d.ReviewRating()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	payDetails(d)
+	payDetails(m)
+}
 func payDetails(p Payer) {
-	fmt.Println(p.Pay())
+	fullName, yearPay := p.Pay()
+	fmt.Printf("%s got paid %.2f for the year\n", fullName, yearPay)
 }
-
-
-
-
-
-
-
-
-
-type triangle struct {
-	base	float64
-	height  float64
+func (d Developer) Pay() (string, float64) {
+	fullName := d.FullName()
+	return fullName, d.HourlyRate * d.HoursWorkedInYear
 }
-
-type rectangle struct {
-	length	float64
-	width  float64
+func (d Developer) FullName() string {
+	fullName := d.Individual.FirstName + " " + d.Individual.LastName
+	return fullName
 }
-type square struct {
-	side float64
+func (d Developer) ReviewRating() error {
+	total := 0
+	for _, v := range d.Review {
+		rating, err := OverallReview(v)
+		if err != nil {
+			return err
+		}
+		total += rating
+	}
+	averageRating := float64(total) / float64(len(d.Review))
+	fmt.Printf("%s got a review rating of %.2f\n",d.FullName(),averageRating)
+	return nil
 }
-
-func (t triangle) Area() float64 {
-	return (t.base * t.height) / 2
+func (m Manager) Pay() (string, float64) {
+	fullName := m.Individual.FirstName + " " + m.Individual.LastName
+	return fullName, m.Salary + (m.Salary * m.CommissionRate)
 }
-
-func (t triangle) Name() string {
-	return "triangle"
-}
-
-func (r rectangle) Area() float64 {
-	return r.length * r.width
-}
-
-func (r rectangle) Name() string {
-	return "rectangle"
-}
-
-func (s square) Area() float64 {
-	return s.side * s.side
-}
-
-func (s square) Name() string {
-	return "square"
-}
-
-func printShapeDetails(shapes ...Shape) {
-	for _, item := range shapes {
-		fmt.Printf("The area of %s is: %.2f\n", item.Name(), item.Area())
+func OverallReview(i interface{}) (int, error) {
+	switch v := i.(type) {
+	case int:
+		return v, nil
+	case string:
+		rating, err := convertReviewToInt(v)
+		if err != nil {
+			return 0, err
+		}
+		return rating, nil
+	default:
+		return 0, errors.New("unknown type")
 	}
 }
-
-func main() {
-	t := triangle{base: 15.5, height: 20.1}
-	r := rectangle{length: 20, width: 10}
-	s := square{side: 10}
-	printShapeDetails(t, r, s)
+func convertReviewToInt(str string) (int, error) {
+	switch str {
+	case "Excellent":
+		return 5, nil
+	case "Good":
+		return 4, nil
+	case "Fair":
+		return 3, nil
+	case "Poor":
+		return 2, nil
+	case "Unsatisfactory":
+		return 1, nil
+	default:
+		return 0, errors.New("invalid rating: " + str)
+	}
 }
