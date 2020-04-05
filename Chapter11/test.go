54e5892bcbe28581e417af6de7f373599732cdff
marat <m.gaynutdinov@mail.ru>
2020-04-05 12:49:20 +0300

    started the 11th chapter

---
+++ Chapter11/test.go
@@ -0,0 +1,11 @@
+package main
+import (
+  "encoding/json"
+  "fmt"
+)
+func main() {
+  jsonData := []byte(`{"checkNum":123,"amount":200,"category":["gift","clothing"]}`)
+  var v interface{}
+  json.Unmarshal(jsonData, &v)
+  fmt.Println(v)
+}
